2056146568028f0957c5d4a10ef6f5682af3b6e0
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-03 22:58:08 +0900

    Update omi.py

--- omi.py
+++ omi.py
@@ -4,3 +4,25 @@ import random
 fortunes = ['大吉', '吉', '凶', '大凶']
 fortune = random.choice(fortunes)
 print(fortune)
+
+
+##追加バージョン
+
+import random
+import time
+
+
+fortunes = ['大吉', '吉', '凶', '大凶']
+fortune = random.choice(fortunes)
+
+print('おみくじを引きます・・・')
+time.sleep(1)
+for i in range(3):
+    if i % 2 == 0:
+        print('ガサッ')
+    else:
+        print('ゴソッ')
+    time.sleep(1)
+print('＿人人人人人人人人人＿')
+print(f'＞{fortune.center(16)}＜')
+print('￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣')

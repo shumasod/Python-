616f5d3a52c75cf88bf4e4d50c2fbc03bc68bc52
shubox <shumasod@gmai.com>
2024-04-29 21:29:01 +0900

    fix code

--- Cisco.py
+++ Cisco.py
@@ -1,4 +1,5 @@
 from ncclient import manager
+import lxml.etree as ET
 
 payload = """
 <config xmlns:xc="urn:ietf:params:xml:ns:netconf:base:1.0" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
@@ -28,19 +29,12 @@ payload = """
         </ip-route-interface-forwarding-list>
       </route>
     </ip>
-</native>
+  </native>
 </config>
 """
 
 # Connect to NETCONF agent on the router
-with manager.connect(
-    host='172.16.62.151',
-    port=830,
-    username='cisco',
-    password='cisco',
-    hostkey_verify=False,
-    device_params={'name': 'iosxe'}
-) as m:
+with manager.connect(host='172.16.62.151', port=830, username='cisco', password='cisco', hostkey_verify=False, device_params={'name': 'iosxe'}) as m:
     print('###########################################################################')
     print('### XML')
     print('###########################################################################')
@@ -49,8 +43,10 @@ with manager.connect(
     print(response)
 
     print('###########################################################################')
-    print('### XML Formatstring')
+    print('### XML Formatted String')
     print('###########################################################################')
+    # Get running configuration
+    running_config = m.get_config(source='running').data_xml
     # Print formatted XML
-    data = m.get_config(source='running').data_xml
-    print(data)
+    data = ET.fromstring(running_config)
+    print(ET.tostring(data, pretty_print=True).decode())

ff72bfed86c192753087c234af8263cde5758916
shubox <shumasod@gmai.com>
2024-05-27 21:41:58 +0900

    omikuji

--- omi.py
+++ omi.py
@@ -1,28 +1,23 @@
 import random
-
-
-fortunes = ['大吉', '吉', '凶', '大凶']
-fortune = random.choice(fortunes)
-print(fortune)
-
-
-##追加バージョン
-
-import random
 import time
 
-
-fortunes = ['大吉', '吉', '凶', '大凶']
-fortune = random.choice(fortunes)
-
-print('おみくじを引きます・・・')
-time.sleep(1)
-for i in range(3):
-    if i % 2 == 0:
-        print('ガサッ')
-    else:
-        print('ゴソッ')
+def draw_fortune():
+    fortunes = ['大吉', '吉', '凶', '大凶']
+    fortune = random.choice(fortunes)
+    
+    print('おみくじを引きます・・・')
     time.sleep(1)
-print('＿人人人人人人人人人＿')
-print(f'＞{fortune.center(16)}＜')
-print('￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣')
+    
+    for i in range(3):
+        if i % 2 == 0:
+            print('ガサッ')
+        else:
+            print('ゴソッ')
+        time.sleep(1)
+        
+    print('＿人人人人人人人人人＿')
+    print(f'＞{fortune.center(16)}＜')
+    print('￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣')
+
+if __name__ == "__main__":
+    draw_fortune()

8afde55db930486a6be5a0a0d5b1d51b007e00b2
ShuBox <64628603+shumasod@users.noreply.github.com>
2025-02-20 13:39:16 +0900

    Update get_ip-route.py.py

--- get_ip-route.py.py
+++ get_ip-route.py.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python
 import requests
 import sys
+import os
 
-# disable warnings from SSL/TLS certificates
+# Disable warnings from SSL/TLS certificates
 requests.packages.urllib3.disable_warnings()
 
-# the IP address or hostname of the networking device
-HOST = '172.16.62.151'
+# The IP address or hostname of the networking device
+HOST = 'privateIP'
 
-# use your user credentials to access the networking device
-USER = 'cisco'
-PASS = 'cisco'
+# Use environment variables to access the networking device credentials
+USER = os.getenv('NETWORK_USER', 'default_user')
+PASS = os.getenv('NETWORK_PASS', 'default_pass')
 PORT = 443
 
-# create a main() method
 def main():
-    """Main method that retrieves the interface details from a
-    networking device via RESTCONF."""
+    """Main method that retrieves the interface details from a networking device via RESTCONF."""
 
-    # RESTCONF url of the networking device
-    # url = "https://{h}:{p}/restconf/data/Cisco-IOS-XE-native:native/ip/route/ip-route-interface-forwarding-list".format(h=HOST, p=PORT)
-    url = "https://{h}:{p}/restconf/data/Cisco-IOS-XE-native:native/ip/route/ip-route-interface-forwarding-list".format(h=HOST, p=PORT)
+    # RESTCONF URL of the networking device
+    url = f"https://{HOST}:{PORT}/restconf/data/Cisco-IOS-XE-native:native/ip/route/ip-route-interface-forwarding-list"
 
     # RESTCONF media types for REST API headers
-    headers = {'Content-Type': 'application/yang-data+json',
-               'Accept': 'application/yang-data+json'}
+    headers = {
+        'Content-Type': 'application/yang-data+json',
+        'Accept': 'application/yang-data+json'
+    }
 
-    # this statement performs a GET on the specified url
-    response = requests.get(url, auth=(USER, PASS),
-                            headers=headers, verify=False)
+    try:
+        # Perform a GET request on the specified URL
+        response = requests.get(url, auth=(USER, PASS), headers=headers, verify=False)
+        response.raise_for_status()  # Raise an exception for HTTP errors
 
-    # print the json that is returned
-    print(response.text)
-    print(response)
+        # Print the JSON that is returned
+        print(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     sys.exit(main())
